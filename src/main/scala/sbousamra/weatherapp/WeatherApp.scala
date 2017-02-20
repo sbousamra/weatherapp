3162717ab7296e42ed71ef981d605d52a5bfebf0
Sebastian Bou-Samra <sebastian.bousamra@hotmail.com>
2017-02-20 15:14:13 +1000

    pseudocode for types in our weatherapp, rough sketch of our approach

--- src/main/scala/sbousamra/weatherapp/WeatherApp.scala
+++ src/main/scala/sbousamra/weatherapp/WeatherApp.scala
@@ -5,3 +5,31 @@ object WeatherApp {
     println("test")
   }
 }
+
+//Pseudocode
+//
+//1. Create method that issues a GET request to a server containing the weather information we want back as JSON.
+//      Will look something like:
+//        request: Request => serverRequestOutput: Response[JSON]
+//
+//2. Now create a method that will take this JSON and filter the data we want, returning our own JSON.
+//      Will look something like:
+//        serverRequestOutput: Response[JSON] => ourWantedJson: JSON
+//
+//3. Create a method that will turn this JSON into a format that is nicely readable for the user.
+//    Will look something like:
+//      ourWantedJson: JSON => niceFormat: NotSureOnType
+//
+//def getDataFromSource(request: Task[Response]): Response[JSON] = {
+//  ???
+//}
+//
+//def ourJson(sourceData: Response[JSON]): JSON = {
+//  ???
+//}
+//
+//def formatForUser(json: JSON) = {
+//  ???
+//}
+
+
