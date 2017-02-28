9305c4e97749db07284a09a02e7e86bc8de62277
Sebastian Bou-Samra <sebastian.bousamra@hotmail.com>
2017-02-28 16:23:48 +1000

    fixed up string interpolation, added cases in the service to catch errors, pulled the http1client outside of the method so its always running, rather then being called each request.

--- src/main/scala/sbousamra/weatherapp/WeatherApp.scala
+++ src/main/scala/sbousamra/weatherapp/WeatherApp.scala
@@ -1,7 +1,6 @@
 package sbousamra.weatherapp
 
 import Types._
-import argonaut._
 import argonaut.Json
 import org.http4s.{HttpService, Uri}
 import org.http4s.client.blaze.PooledHttp1Client
@@ -11,26 +10,27 @@ import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.argonaut._
 import org.http4s.client.Client
 
+import scalaz.{-\/, \/-}
 import scalaz.concurrent.Task
 
 object WeatherApp extends ServerApp {
+  val httpClient = PooledHttp1Client()
 
-  def getWeatherApi(request: WeatherForecastRequest, httpClient: Client): Task[Json] = {
+  def getWeatherApi(request: WeatherForecastRequest, client: Client): Task[Json] = {
     val uri = Uri
       .unsafeFromString("https://query.yahooapis.com/v1/public/yql")
-      .withQueryParam("q", "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=" + """"""" + request.location + """"""" + ")")
+      .withQueryParam("q", s"""select * from weather.forecast where woeid in (select woeid from geo.places(1) where text= "${request.location}")""")
       .withQueryParam("format", "json")
-    println(uri)
-    val weatherRequest = httpClient.expect[Json](uri)
-    weatherRequest
+    httpClient.expect[Json](uri)
   }
 
   def getRoute: HttpService = {
     HttpService {
       case GET -> Root / location => {
-        val weatherApiTask = getWeatherApi(WeatherForecastRequest(1, "gold coast"), PooledHttp1Client()).attempt.run
-        val weatherApiTest = weatherApiTask.toString
-        Ok(weatherApiTest)
+        getWeatherApi(WeatherForecastRequest(1, "brisbane"), httpClient).attempt.flatMap {
+          case \/-(json) => Ok(json.spaces2)
+          case -\/(err) => InternalServerError(err.toString)
+        }
       }
     }
   }
