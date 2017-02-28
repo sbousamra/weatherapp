f9ed3deb9b6d02bf3a5c42dff566d62958500725
Sebastian Bou-Samra <sebastian.bousamra@hotmail.com>
2017-02-28 15:44:38 +1000

    added yqlquery to yahoo api returning json of all weather data. Requires user to input location so far, and will return weather data for that location.

--- src/main/scala/sbousamra/weatherapp/WeatherApp.scala
+++ src/main/scala/sbousamra/weatherapp/WeatherApp.scala
@@ -1,28 +1,34 @@
 package sbousamra.weatherapp
 
 import Types._
-import argonaut._, argonaut.Json
-import org.http4s.HttpService
+import argonaut._
+import argonaut.Json
+import org.http4s.{HttpService, Uri}
 import org.http4s.client.blaze.PooledHttp1Client
 import org.http4s.dsl._
 import org.http4s.server.{Server, ServerApp}
 import org.http4s.server.blaze.BlazeBuilder
-import org.http4s.argonaut.ArgonautInstances._
+import org.http4s.argonaut._
+import org.http4s.client.Client
 
 import scalaz.concurrent.Task
 
 object WeatherApp extends ServerApp {
 
-  def getWeatherApi(request: WeatherForecastRequest): Task[Json] = {
-    val httpClient = PooledHttp1Client()
-    val weatherRequest = httpClient.expect[Json]("https://www.metaweather.com/api/location/44418/")
+  def getWeatherApi(request: WeatherForecastRequest, httpClient: Client): Task[Json] = {
+    val uri = Uri
+      .unsafeFromString("https://query.yahooapis.com/v1/public/yql")
+      .withQueryParam("q", "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=" + """"""" + request.location + """"""" + ")")
+      .withQueryParam("format", "json")
+    println(uri)
+    val weatherRequest = httpClient.expect[Json](uri)
     weatherRequest
   }
 
   def getRoute: HttpService = {
     HttpService {
       case GET -> Root / location => {
-        val weatherApiTask = getWeatherApi(WeatherForecastRequest(1, "test")).attempt.run
+        val weatherApiTask = getWeatherApi(WeatherForecastRequest(1, "gold coast"), PooledHttp1Client()).attempt.run
         val weatherApiTest = weatherApiTask.toString
         Ok(weatherApiTest)
       }
